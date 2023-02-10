5b704d07c079e2ff5caa61894e823f166b7fcb5c
Junji Tamaki <junji.tamaki@exmotion.co.jp>
2023-02-10 10:35:11 +0900

    add test

--- test/test.py
+++ test/test.py
@@ -1,3 +1,22 @@
-#!/bin/bash
+import socket
+import time
 
-python3 test.py
+def main():
+    serv_address = ('127.0.0.1', 10000)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    send_len = sock.sendto('t'.encode('utf-8'), serv_address)
+    time.sleep(5)
+    send_len = sock.sendto('w'.encode('utf-8'), serv_address)
+    time.sleep(5)
+    send_len = sock.sendto('s'.encode('utf-8'), serv_address)
+    time.sleep(5)
+    send_len = sock.sendto('a'.encode('utf-8'), serv_address)
+    time.sleep(5)
+    send_len = sock.sendto('d'.encode('utf-8'), serv_address)
+    time.sleep(5)
+    send_len = sock.sendto('q'.encode('utf-8'), serv_address)
+    time.sleep(5)
+
+if __name__ == '__main__':
+    main()
